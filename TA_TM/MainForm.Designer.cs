28c07483fa193e240ccad4a0504fac88b200d0f9
takandrew <andreikaril@mail.ru>
2021-06-08 23:36:17 +0300

    Первый коммит

---
+++ TA_TM/MainForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace TA_TM
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.AlphabetTextBox = new System.Windows.Forms.TextBox();
+            this.AlphabetButton = new System.Windows.Forms.Button();
+            this.TableGridView = new System.Windows.Forms.DataGridView();
+            this.AddColumnTableButton = new System.Windows.Forms.Button();
+            this.DeleteColumnTableButton = new System.Windows.Forms.Button();
+            this.CommandGridView = new System.Windows.Forms.DataGridView();
+            this.StartButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.TableGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CommandGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(424, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Алфавит";
+            // 
+            // AlphabetTextBox
+            // 
+            this.AlphabetTextBox.Location = new System.Drawing.Point(366, 38);
+            this.AlphabetTextBox.Name = "AlphabetTextBox";
+            this.AlphabetTextBox.Size = new System.Drawing.Size(180, 22);
+            this.AlphabetTextBox.TabIndex = 1;
+            // 
+            // AlphabetButton
+            // 
+            this.AlphabetButton.Location = new System.Drawing.Point(381, 66);
+            this.AlphabetButton.Name = "AlphabetButton";
+            this.AlphabetButton.Size = new System.Drawing.Size(153, 44);
+            this.AlphabetButton.TabIndex = 3;
+            this.AlphabetButton.Text = "Записать алфавит";
+            this.AlphabetButton.UseVisualStyleBackColor = true;
+            this.AlphabetButton.Click += new System.EventHandler(this.AlphabetButton_Click);
+            // 
+            // TableGridView
+            // 
+            this.TableGridView.AllowUserToAddRows = false;
+            this.TableGridView.AllowUserToDeleteRows = false;
+            this.TableGridView.AllowUserToResizeColumns = false;
+            this.TableGridView.AllowUserToResizeRows = false;
+            this.TableGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.TableGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.TableGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TableGridView.Location = new System.Drawing.Point(24, 227);
+            this.TableGridView.Name = "TableGridView";
+            this.TableGridView.RowHeadersVisible = false;
+            this.TableGridView.RowHeadersWidth = 51;
+            this.TableGridView.RowTemplate.Height = 24;
+            this.TableGridView.Size = new System.Drawing.Size(705, 178);
+            this.TableGridView.TabIndex = 4;
+            // 
+            // AddColumnTableButton
+            // 
+            this.AddColumnTableButton.Location = new System.Drawing.Point(751, 252);
+            this.AddColumnTableButton.Name = "AddColumnTableButton";
+            this.AddColumnTableButton.Size = new System.Drawing.Size(149, 50);
+            this.AddColumnTableButton.TabIndex = 5;
+            this.AddColumnTableButton.Text = "Добавить столбец";
+            this.AddColumnTableButton.UseVisualStyleBackColor = true;
+            this.AddColumnTableButton.Click += new System.EventHandler(this.AddColumnTableButton_Click);
+            // 
+            // DeleteColumnTableButton
+            // 
+            this.DeleteColumnTableButton.Location = new System.Drawing.Point(751, 319);
+            this.DeleteColumnTableButton.Name = "DeleteColumnTableButton";
+            this.DeleteColumnTableButton.Size = new System.Drawing.Size(149, 50);
+            this.DeleteColumnTableButton.TabIndex = 6;
+            this.DeleteColumnTableButton.Text = "Удалить столбец";
+            this.DeleteColumnTableButton.UseVisualStyleBackColor = true;
+            this.DeleteColumnTableButton.Click += new System.EventHandler(this.DeleteColumnTableButton_Click);
+            // 
+            // CommandGridView
+            // 
+            this.CommandGridView.AllowUserToAddRows = false;
+            this.CommandGridView.AllowUserToDeleteRows = false;
+            this.CommandGridView.AllowUserToResizeColumns = false;
+            this.CommandGridView.AllowUserToResizeRows = false;
+            this.CommandGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.CommandGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.CommandGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CommandGridView.ColumnHeadersVisible = false;
+            this.CommandGridView.Location = new System.Drawing.Point(24, 120);
+            this.CommandGridView.Name = "CommandGridView";
+            this.CommandGridView.RowHeadersVisible = false;
+            this.CommandGridView.RowHeadersWidth = 51;
+            this.CommandGridView.RowTemplate.Height = 24;
+            this.CommandGridView.Size = new System.Drawing.Size(705, 84);
+            this.CommandGridView.TabIndex = 7;
+            this.CommandGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.CommandGridView_CellClick);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(381, 424);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(153, 44);
+            this.StartButton.TabIndex = 8;
+            this.StartButton.Text = "Запуск";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 493);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.CommandGridView);
+            this.Controls.Add(this.DeleteColumnTableButton);
+            this.Controls.Add(this.AddColumnTableButton);
+            this.Controls.Add(this.TableGridView);
+            this.Controls.Add(this.AlphabetButton);
+            this.Controls.Add(this.AlphabetTextBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "Машина Тьюринга";
+            ((System.ComponentModel.ISupportInitialize)(this.TableGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CommandGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox AlphabetTextBox;
+        private System.Windows.Forms.Button AlphabetButton;
+        private System.Windows.Forms.DataGridView TableGridView;
+        private System.Windows.Forms.Button AddColumnTableButton;
+        private System.Windows.Forms.Button DeleteColumnTableButton;
+        private System.Windows.Forms.DataGridView CommandGridView;
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ TA_TM/MainForm.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TA_TM
+{
+    public partial class MainForm : Form
+    {
+        List<object> alphabetList = new List<object>();
+        int currentColumn = 1;
+        public MainForm()
+        {
+            InitializeComponent();
+            AddColumnTableButton.Enabled = false;
+            DeleteColumnTableButton.Enabled = false;
+            StartButton.Enabled = false;
+            MaximizeBox = false;
+        }
+
+        private bool GetData(TextBox textBox, List<object> dataList)
+        {
+            try
+            {
+                string dataString = textBox.Text;
+                string[] splittedDataString = dataString.Split(' ');
+                int zero = 0;
+                int countSpaces = zero;
+                for (int i = 0; i < splittedDataString.Length; i++)
+                {
+                    if (splittedDataString[i] != "")
+                    {
+                        countSpaces++;
+                    }
+                }
+                if (countSpaces == zero)
+                {
+                    MessageBox.Show("Исходные данные отсутствуют", "Внимание!");
+                    return false;
+                }
+                for (int i = 0; i < splittedDataString.Length; i++)
+                {
+                    for (int k = i+1; k < splittedDataString.Length; k++)
+                    {
+                        if (splittedDataString[i] == splittedDataString[k])
+                        {
+                            splittedDataString[i] = "";
+                            countSpaces--;
+                            break;
+                        }
+                    }
+                }
+                string[] withoutSpacesDataString = new string[countSpaces];
+                int j = 0;
+                for (int i = 0; i < splittedDataString.Length; i++)
+                {
+                    if (splittedDataString[i] != "")
+                    {
+                        withoutSpacesDataString[j] = splittedDataString[i];
+                        j++;
+                    }
+                }
+                for (int i = 0; i < withoutSpacesDataString.Length; i++)
+                {
+                    dataList.Add(withoutSpacesDataString[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка ввода");
+                return false;
+            }
+            return true;
+        }
+
+        private void AlphabetButton_Click(object sender, EventArgs e)
+        {
+            alphabetList.Clear();
+            TableGridView.Rows.Clear(); TableGridView.Columns.Clear();
+            CommandGridView.Rows.Clear(); CommandGridView.Columns.Clear();
+            if (GetData(AlphabetTextBox, alphabetList))
+            {
+                AddColumnTableButton.Enabled = true;
+                DeleteColumnTableButton.Enabled = true;
+                StartButton.Enabled = true;
+                TableGridView.Columns.Add("", "");
+                TableGridView.Columns.Add("Q1", "Q1");
+                for (int i = 0; i < alphabetList.Count; i++)
+                    TableGridView.Rows.Add(alphabetList[i]);
+                TableGridView.Rows.Add("");
+                for (int i = -alphabetList.Count*10; i <= alphabetList.Count*10; i++)
+                    CommandGridView.Columns.Add("", "");
+                CommandGridView.Rows.Add("", "");
+                CommandGridView.Rows.Add("", "");
+            }
+            else
+            {
+                AddColumnTableButton.Enabled = false;
+                DeleteColumnTableButton.Enabled = false;
+                StartButton.Enabled = false;
+            }
+        }
+
+        private void AddColumnTableButton_Click(object sender, EventArgs e)
+        {
+            string columnName = "Q" + TableGridView.Columns.Count;
+            TableGridView.Columns.Add(columnName, columnName);
+        }
+
+        private void DeleteColumnTableButton_Click(object sender, EventArgs e)
+        {
+            if (TableGridView.Columns.Count != 2)
+            {
+                string columnName = "Q" + (TableGridView.Columns.Count-1);
+                TableGridView.Columns.Remove(columnName);
+            }
+        }
+
+        private void CommandGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            for (int j = 0; j < CommandGridView.Rows[0].Cells.Count; j++)
+                CommandGridView.Rows[0].Cells[j].Value = null;
+            int i = CommandGridView.CurrentCell.ColumnIndex;
+            CommandGridView.Rows[0].Cells[i].Value = "*";
+            currentColumn = 1;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            int pointer = -1;
+            for (int i = 0; i < CommandGridView.Rows[0].Cells.Count; i++)
+                if ((CommandGridView.Rows[0].Cells[i].Value != null)
+                    && (CommandGridView.Rows[0].Cells[i].Value.ToString() == "*"))
+                {
+                    pointer = i;
+                    break;
+                }
+            if (pointer != -1)
+            {
+                object currentState = CommandGridView.Rows[1].Cells[pointer].Value;
+                if (!alphabetList.Contains(currentState))
+                {
+                    MessageBox.Show("Лента содержит символ, не содержащийся в алфавите", "Ошибка!");
+                    return;
+                }
+                int currentIndex = -1;
+                for (int i = 0; i < TableGridView.RowCount; i++)
+                {
+                    if (TableGridView.Rows[i].Cells[0].Value.ToString() == currentState.ToString())
+                    {
+                        currentIndex = i;
+                        break;
+                    }
+                }
+                if (TableGridView.Rows[currentIndex].Cells[currentColumn].Value == null)
+                {
+                    MessageBox.Show("Ячейка не содержит команды", "Ошибка!");
+                    return;
+                }
+                TableGridView.Rows[currentIndex].Cells[currentColumn].Selected = true;
+                string[] currentCommand = TableGridView.Rows[currentIndex].Cells[currentColumn].Value.ToString().Split(' ');
+                if (currentCommand.Length != 3)
+                {
+                    MessageBox.Show("Команда введена некорректно", "Ошибка!");
+                    return;
+                }
+                if (!alphabetList.Contains(currentCommand[0]))
+                {
+                    MessageBox.Show("Команда введена некорректно", "Ошибка!");
+                    return;
+                }
+                CommandGridView.Rows[1].Cells[pointer].Value = currentCommand[0];
+                if (currentCommand[1] == "R")
+                {
+                    if (pointer != CommandGridView.Columns.Count)
+                    {
+                        CommandGridView.Rows[0].Cells[pointer].Value = "";
+                        CommandGridView.Rows[0].Cells[pointer + 1].Value = "*";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Команда введена некорректно. \n Ячейка является крайней справа.", "Ошибка!");
+                        return;
+                    }
+                }
+                else if (currentCommand[1] == "L")
+                {
+                    if (pointer != 0)
+                    {
+                        CommandGridView.Rows[0].Cells[pointer].Value = "";
+                        CommandGridView.Rows[0].Cells[pointer - 1].Value = "*";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Команда введена некорректно. \n Ячейка является крайней слева.", "Ошибка!");
+                        return;
+                    }
+                }
+                else if (currentCommand[1] == "S")
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("Корректный вид команды в ячейке: \n " +
+                        "значение, на которое нужно заменить (должно содержаться в алфавите) \n" +
+                        "смещение (L - влево, R - вправо, S - остаться на месте) \n" +
+                        "номер состояния (Если нужно Q2, то пишем 2)", "Ошибка!");
+                    return;
+                }
+                currentColumn = Convert.ToInt32(currentCommand[2]);
+                if ((currentColumn <= 0) && (currentColumn > TableGridView.Columns.Count))
+                {
+                    MessageBox.Show("Команда введена некорректно. \n Состояние некорректно", "Ошибка!");
+                    currentColumn = 1;
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Необходимо установить указатель ленты на начало выполнения команды", "Ошибка!");
+            }
+        }
+    }
+}
